a2a06e4b27f51c7a1a6ef49e2ba4f6fd32aaf987
billybobbonnet <boitalettre9@gmail.com>
2020-11-25 12:50:26 +0100

    lintfix

--- .nuxt/App.js
+++ .nuxt/App.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 
 import { getMatchedComponentsInstances, getChildrenComponentInstancesUsingFetch, promisify, globalHandleError, urlJoin, sanitizeComponent } from './utils'
-import NuxtError from '../layouts/error.vue'
+import NuxtError from '..\\layouts\\error.vue'
 import NuxtLoading from './components/nuxt-loading.vue'
+import NuxtBuildIndicator from './components/nuxt-build-indicator'
 
-import _6f6c098b from '../layouts/default.vue'
+import _6f6c098b from '..\\layouts\\default.vue'
 
 const layouts = { "_default": sanitizeComponent(_6f6c098b) }
 
@@ -41,7 +42,7 @@ export default {
       }
     }, [
       loadingEl,
-
+      h(NuxtBuildIndicator),
       transitionEl
     ])
   },
@@ -181,6 +182,10 @@ export default {
     },
 
     setLayout (layout) {
+      if(layout && typeof layout !== 'string') {
+        throw new Error('[nuxt] Avoid using non-string value as layout property.')
+      }
+
       if (!layout || !layouts['_' + layout]) {
         layout = 'default'
       }
--- root.config.js
+++ root.config.js
@@ -1,11 +1,13 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
-  name: "wprn",
-  full_name: "World Pandemic Researh Network", // set to false if there is none
-  description: "Public initiative to publish and share in one single place the list of all research initiatives and surveys about the current pandemic of Covid-19 coronavirus.",
-  recaptcha: "6Lc_xuUUAAAAALwowUq0cC0wFtFnZ2NCi3UH4i1j",
-  graphqlEndpoint: "https://skbd6n5nynecvdhgjiisxmyer4.appsync-api.eu-west-2.amazonaws.com/graphql",
+  name: 'wprn',
+  full_name: 'World Pandemic Researh Network', // set to false if there is none
+  description:
+    'Public initiative to publish and share in one single place the list of all research initiatives and surveys about the current pandemic of Covid-19 coronavirus.',
+  recaptcha: '6Lc_xuUUAAAAALwowUq0cC0wFtFnZ2NCi3UH4i1j',
+  graphqlEndpoint:
+    'https://skbd6n5nynecvdhgjiisxmyer4.appsync-api.eu-west-2.amazonaws.com/graphql',
   isBranch: false,
   url: 'https://wprn.org',
   fields: {
@@ -24,17 +26,17 @@ export default {
     team: true,
     cite: true,
     listReferents: true,
-    showAll: true
+    showAll: true,
   },
   features: {
     theme: true,
     referents: true,
     publicAlerts: true,
-    projetAlters: true
+    projetAlters: true,
   },
   font: {
     family: 'String',
-    fallback: 'String'
+    fallback: 'String',
   },
   theme: {
     dark: false,
@@ -46,7 +48,7 @@ export default {
         error: '#FF5252',
         info: '#2196F3',
         success: '#4CAF50',
-        warning: '#FFC107'
+        warning: '#FFC107',
       },
       dark: {
         primary: colors.blue,
@@ -55,9 +57,8 @@ export default {
         info: colors.teal.lighten1,
         warning: colors.amber.darken3,
         error: colors.deepOrange.darken3,
-        success: colors.green.accent3
-      }
-    }
-  }
-
+        success: colors.green.accent3,
+      },
+    },
+  },
 }
