dc0e5e6eb79101916d2b86f8ecd4822c47d86fbe
Billybobbonnet <boitalettre9@gmail.com>
2020-12-15 14:36:22 +0100

    fix: static prefix

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,6 +7,9 @@ const routerBase =
         router: {
           base: '/Positive-Future/',
         },
+        static: {
+          prefix: '/Positive-Future/',
+        },
       }
     : {}
 export default {
