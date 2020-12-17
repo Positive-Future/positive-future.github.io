c9a53ecbe6631173c21894c396d03a07a1e18324
Billybobbonnet <boitalettre9@gmail.com>
2020-12-17 16:30:44 +0100

    fix: logo

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <v-card-text class="pa-sm-12">
     <v-row>
       <v-col v-if="$vuetify.breakpoint.mdAndUp" cols="2" class="pl-7">
-        <v-img src="$router.options.base + /logo.svg" contain></v-img>
+        <v-img :src="$router.options.base + '/logo.svg'" contain></v-img>
       </v-col>
       <v-col cols="12" md="10" class="pl-6">
         <div class="text-h2 my-3">
