95b7ef1fdcc5a0f9b5dc23b15a4376efa49a2139
billybobbonnet <boitalettre9@gmail.com>
2021-03-16 09:22:11 +0000

    [skip ci] Updates

---
+++ _nuxt/f85dcf8.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{265:function(n,e,o){"use strict";o.r(e),e.default=[{code:"fr",iso:"fr-FR",name:"French",file:"fr.json",dir:"ltr"},{code:"en",iso:"en-US",name:"English",file:"en.json",dir:"ltr"}]}}]);
--- lang/languages.js
+++ lang/languages.js
@@ -3,14 +3,14 @@ export default [
     code: 'fr',
     iso: 'fr-FR',
     name: 'French',
-    file: 'fr-FR.js',
+    file: 'fr.json',
     dir: 'ltr',
   },
   {
     code: 'en',
     iso: 'en-US',
     name: 'English',
-    file: 'en-US.js',
+    file: 'en.json',
     dir: 'ltr',
   },
 ]
