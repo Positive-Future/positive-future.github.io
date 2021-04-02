e96da8c9183210571942bd8d01fd577136d93a4e
Billybobbonnet <boitalettre9@gmail.com>
2021-04-02 15:20:06 +0200

    fix: rules file

--- pages/contest/rules.vue
+++ pages/contest/rules.vue
@@ -37,7 +37,11 @@
                 class="pa-6"
                 flat
                 target="_blank"
-                :to="'/rules_' + $i18n.locale + '.pdf'"
+                :to="
+                  'https://www.positive-future.org/rules_' +
+                  $i18n.locale +
+                  '.pdf'
+                "
                 :class="{ 'ml-n6': $vuetify.breakpoint.mdAndUp }"
               >
                 <div class="d-inline-flex justify-center align-center">
