7e8578114301020c651a3c1185c577382bc7eef8
Billybobbonnet <boitalettre9@gmail.com>
2021-04-08 09:28:32 +0200

    feat: jury list

---
+++ content/fr/jury/blauel.md
@@ -0,0 +1,5 @@
+---
+name: Célia Blauel
+description: est adjointe à la Maire de Paris depuis 2014. Après un mandat chargée des dossiers concernant la Transition écologique, le Climat et l’Eau, Célia Blauel est depuis  les élections municipales de juin 2020, chargée de la Seine, de la Prospective Paris 2030 et de la Résilience.
+
+---
--- pages/contest/jury.vue
+++ pages/contest/jury.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <section
-      style="background-color: #fff1d0; padding-bottom: 80px"
+      style="background-color: #fff1d0; padding-bottom: 40px"
       :class="{ 'px-3': $vuetify.breakpoint.smAndDown }"
     >
       <v-row justify="center">
@@ -15,15 +15,33 @@
         </v-col>
       </v-row>
     </section>
+    <section :class="{ 'px-3': $vuetify.breakpoint.smAndDown }">
+      <v-row justify="center">
+        <v-col xs="12" sm="11" md="8" lg="7" xl="6">
+          <template v-for="(item, index) in list">
+            <div :key="index" class="my-6">
+              <span class="name px-1">
+                {{ item.name }}
+              </span>
+              <p>{{ item.description }}</p>
+            </div>
+          </template>
+        </v-col>
+      </v-row>
+    </section>
   </div>
 </template>
 <script>
 export default {
   async asyncData({ app, $content }) {
+    const list = await $content(app.i18n.locale + '/jury').fetch()
     const jury = await $content(app.i18n.locale + '/pages/jury').fetch()
+
+    console.log('list: ', list)
 
     return {
       jury,
+      list,
     }
   },
   data() {
@@ -33,3 +51,9 @@ export default {
   methods: {},
 }
 </script>
+<style>
+.name {
+  background-color: black;
+  color: white;
+}
+</style>
