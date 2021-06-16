5ba43685a5f4811fe1e227bd73f7c17e83b0a959
Billybobbonnet <Billybobbonnet@users.noreply.github.com>
2021-06-16 14:37:36 +0000

    deploy: 18a1b03014167d011f225dcf1d3f067467af5c32

--- _nuxt/68e8901.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{265:function(n,e,o){"use strict";o.r(e),e.default=[{code:"fr",iso:"fr-FR",name:"French",file:"fr.json",dir:"ltr"},{code:"en",iso:"en-US",name:"English",file:"en.json",dir:"ltr"}]}}]);
