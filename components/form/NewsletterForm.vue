80c52a995a4f9078ab0003c6215b9d6a66e22b13
Billybobbonnet <boitalettre9@gmail.com>
2021-06-16 17:56:03 +0200

    fix: damnit

--- components/form/NewsletterForm.vue
+++ components/form/NewsletterForm.vue
@@ -105,12 +105,7 @@
                 </div>
                 <div
                   id="mergeRow-gdpr"
-                  class="
-                    mergeRow
-                    gdpr-mergeRow
-                    content__gdprBlock
-                    mc-field-group
-                  "
+                  class="mergeRow gdpr-mergeRow content__gdprBlock mc-field-group"
                 >
                   <div class="content__gdpr">
                     <label>Permission</label>
