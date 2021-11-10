a75e7a1c99dfdc3fe3e9aea8a82658518c2f1c37
Billybobbonnet <boitalettre9@gmail.com>
2021-11-10 18:04:06 +0100

    fix: translation

---
+++ components/news/NewsItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-hover v-slot="{ hover }">
+    <v-card
+      nuxt
+      :to="localePath('/news/' + item.slug)"
+      color="#FFF1D0"
+      :elevation="hover ? 12 : 2"
+    >
+      <OptimizedImage :src="item.image"> </OptimizedImage>
+      <v-card-title style="word-break: normal"> {{ item.title }} </v-card-title>
+      <v-card-text>
+        {{ item.subtitle }}
+      </v-card-text>
+    </v-card>
+  </v-hover>
+</template>
+<script>
+import { formatDistance } from 'date-fns'
+
+export default {
+  props: {
+    item: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  computed: {
+    dateAgo() {
+      const date = new Date(this.item.createdAt)
+
+      return formatDistance(date, new Date()) + ' ago'
+    },
+  },
+}
+</script>
