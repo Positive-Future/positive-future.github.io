a5091a2fc82fd23ea86a6744742daa2f82a763b0
Billybobbonnet <Billybobbonnet@users.noreply.github.com>
2022-01-26 14:23:00 +0000

    deploy: 2350b7fbf36cb2359f72a273c3783f8ed66b0914

---
+++ _nuxt/static/1643206924/resources/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643206924",layout:"default",error:a,state:{offsetTop:0,tab:"",scrolling:b,contactOnly:b,i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fresources",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bf4af3bc"}},__i18n:{langs:{}}}}(null,false));
