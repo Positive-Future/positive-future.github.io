2b3d2d6948aa19f4f075207992ca56ef28a0badf
Billybobbonnet <boitalettre9@gmail.com>
2022-02-25 11:42:54 +0100

    fix: order redirect field missing

--- middleware/redirect.js
+++ middleware/redirect.js
@@ -1,16 +1,21 @@
-export default async function ({ route, store, $content, redirect, app }) {
+export default async function ({ route, $content, redirect, app }) {
   if (route.name.startsWith('redirect-lang-id')) {
     if (route.params.lang !== app.i18n.locale)
       await app.i18n.setLocale(route.params.lang)
     const original = await $content(app.i18n.locale + '/webinars')
       .where({ order: +route.params.id })
       .fetch()
-
+    console.log('original: ', original)
+    console.log(
+      'app.i18n.locale',
+      route.params.lang === 'en' ? '' : '/fr' + '/webinars/' + original[0].slug
+    )
+    console.log('original.length: ', original.length)
     return original.length
       ? redirect(
-          app.i18n.locale === 'en'
-            ? ''
-            : '/fr' + '/webinars/' + original[0].slug
+          route.params.lang === 'fr'
+            ? '/fr'
+            : '' + '/webinars/' + original[0].slug
         )
       : true
   }
