c31a3216119e0926bc0588f52c3afacdbfc6b16a
Billybobbonnet <Billybobbonnet@users.noreply.github.com>
2022-09-30 11:46:55 +0000

    deploy: 347da48d037d7e8d8257ae323da71090eb8a00db

---
+++ _nuxt/be64c76.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[37],{597:function(n,t,e){"use strict";e.r(t);var o={middleware:"redirect",props:{},data:function(){return{}},computed:{},mounted:function(){},methods:{}},r=e(37),component=Object(r.a)(o,(function(){return(0,this._self._c)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
