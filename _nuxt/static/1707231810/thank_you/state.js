0d04d819b0a5460edf5a4694ba93eba2780ae570
Solenedb <Solenedb@users.noreply.github.com>
2024-02-06 15:25:53 +0000

    deploy: d6fc7793edfe29805092c60800741cd880df19ce

--- _nuxt/static/1707231810/thank_you/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1707231810",layout:"default",error:a,state:{offsetTop:0,tab:"",scrolling:b,contactOnly:b,i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fthank_you",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9f1b0981"}},__i18n:{langs:{}}}}(null,false));
