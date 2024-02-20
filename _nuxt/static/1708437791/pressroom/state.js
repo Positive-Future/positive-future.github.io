14e2b2c9fb20b8ee9e1ab2dcbaa3e87f5960ac91
Solenedb <Solenedb@users.noreply.github.com>
2024-02-20 14:03:40 +0000

    deploy: e30d72ee3182e8b9bf07639e668c2b79f77ba8f1

---
+++ _nuxt/static/1708437791/pressroom/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1708437791",layout:"default",error:a,state:{offsetTop:0,tab:"",scrolling:b,contactOnly:b,i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fpressroom",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3481ec0f"}},__i18n:{langs:{}}}}(null,false));
