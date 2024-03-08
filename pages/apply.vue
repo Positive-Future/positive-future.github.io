73d1e1707a7e3cb91bcebd5760e00b77a33e4c89
CI <username@users.noreply.github.com>
2024-03-08 10:03:46 +0100

    fix: linter excception for apply page regexes

--- pages/apply.vue
+++ pages/apply.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable no-useless-escape -->
 <template>
   <div>
     <section style="background-color: #fff1d0">
@@ -402,10 +403,11 @@
   </div>
 </template>
 <script>
-// eslint-disable-next-line no-useless-escape
 const url =
+  // eslint-disable-next-line no-useless-escape
   /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/
 const email =
+  // eslint-disable-next-line no-useless-escape
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 export default {
   beforeRouteLeave(to, from, next) {
